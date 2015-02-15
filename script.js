c937efbf1e9f36615b356222b175a5abf255f337
Andrew Li <andrew.li@tufts.edu>
2015-02-14 20:20:37 -0500

    testing

--- script.js
+++ script.js
@@ -201,14 +201,14 @@ function initialize() {
     };
     var map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
-    for (i = 0; i < listOfSites.cities.length; i++) { // Create LatLng object for each site
+    /*for (i = 0; i < listOfSites.cities.length; i++) { // Create LatLng object for each site
         cs_latlng.push(new google.maps.LatLng(listOfSites.cities[i].lat, 
             listOfSites.cities[i].lng));
         cs_marker.push(new google.maps.Marker({
                         position: cs_latlng[i],
                         map: map,
                         title: listOfSites.cities[i].cname}));
-    }
+    }*/
 
 }
 
