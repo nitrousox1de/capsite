4a5b523190cc5d9d494d43036169a57e2d67aceb
Andrew Li <andrew.li@tufts.edu>
2015-02-14 20:41:02 -0500

    initialize name

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-      function initialize() {
-        var mapOptions = {
-          center: { lat: 42.364506, lng: -71.038887},
-          zoom: 8
-        };
-        var map = new google.maps.Map(document.getElementById('map-canvas'),
-            mapOptions);
-      }
-      google.maps.event.addDomListener(window, 'load', initialize);
-      
+function initialize() {
+    var mapOptions = {
+      center: { lat: 42.364506, lng: -71.038887},
+      zoom: 8
+    };
+    var map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+}
+function mapLoad(){
+    google.maps.event.addDomListener(window, 'load', initialize);
+}
+
 /*
 function initialize() {
     var mapOptions = {
